4653b3a9e18bb8e432c93a50e432b39a29b0d106
RobbyQuintiens <robby.quintiens@hotmail.com>
2021-11-17 14:14:44 +0100

    Added basic welcomescreen

---
+++ idCardXF/idCardXF/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using idCardXF.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace idCardXF
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new HomeView();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
